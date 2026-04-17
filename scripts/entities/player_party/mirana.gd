57372d9aa7f7215de0ec44afdf0a0e7322fef045
luizpanigassi <luiz.panigassi@gmail.com>
2026-04-17 15:12:30 -0300

    [fix]: player.tscn hidden

--- scripts/entities/player_party/mirana.gd
+++ scripts/entities/player_party/mirana.gd
@@ -4,6 +4,7 @@ class_name Mirana
 func _build_loadout():
 	actions.clear()
 	actions.append(AttackAction.new())
+	actions.append(SkillFactory.fireball())
 	actions.append(SkillFactory.healing())
 	actions.append(SkillFactory.regen())
 	actions.append(SkillFactory.meditate())
